d7968fa2723a66f9c1ab2dfef45a4dd99f6c651e
DATA SOURCE <sourced2929@gmail.com>
2025-07-31 15:26:36 +0000

    after scrolling till the end its creating gap at the end make it scroll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,7 +99,7 @@ export default {
         },
         'scroll': {
           '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(calc(-100% - 2rem))' },
+          '100%': { transform: 'translateX(calc(-50%))' },
         }
       },
       animation: {
