6c9c56547bd98394d74947f43467bf1dab04ef63
DATA SOURCE <sourced2929@gmail.com>
2025-08-05 14:12:10 +0000

    onhover remove this background only the main object should be apper

--- src/components/team-card.tsx
+++ src/components/team-card.tsx
@@ -2,8 +2,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-import { Linkedin, Twitter, Facebook } from "lucide-react";
 
 interface TeamCardProps {
     name: string;
