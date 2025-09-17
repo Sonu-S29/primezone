76a9c876cf80731db733399e9b057c7e14c77240
DATA SOURCE <sourced2929@gmail.com>
2025-09-17 13:35:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,11 @@ const nextConfig: NextConfig = {
 
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      "https://*.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev",
+    ],
+  },
 };
 
 export default nextConfig;
