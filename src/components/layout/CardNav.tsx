9d4ce00f51b56c22e6f634f1c2ad2b8ed655aea8
DATA SOURCE <sourced2929@gmail.com>
2025-11-20 18:24:15 +0000

    the animation is not visible ??

--- src/components/layout/CardNav.tsx
+++ src/components/layout/CardNav.tsx
@@ -60,8 +60,7 @@ const CardNav = ({
             </Link>
           </div>
           <div className="animated-text-container">
-            <h1
-                className="text-lg md:text-xl lg:text-2xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+            <h1 className="text-lg md:text-xl lg:text-2xl font-semibold">
                 <Cover>Primezone</Cover>
             </h1>
           </div>
