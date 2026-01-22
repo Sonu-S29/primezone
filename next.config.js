fa9de0912ff544b8e24b8a1de07d4f0caa3b6ce7
DATA SOURCE <sourced2929@gmail.com>
2026-01-22 15:11:36 +0000

    still when clicking on { import { MetadataRoute } from 'next'
    export d

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',              
   images: {
-    unoptimized: true,            
     deviceSizes: [640, 768, 1024, 1280, 1536],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
