e910fb60aab376a70f48c1c94f81290f120b06fe
DATA SOURCE <sourced2929@gmail.com>
2026-01-23 09:46:59 +0000

    The Netlify deploy errored, with the following guidance provided:
    Diagn

--- src/app/robots.ts
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left empty to prevent conflicts with the static robots.txt in the public folder.
--- src/app/sitemap.ts
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left empty to prevent conflicts with the static sitemap.xml in the public folder.
